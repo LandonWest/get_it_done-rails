0d2a33952099245f4e3aba7764a9f94fe98f9ee8
Landon West <landonwest5@gmail.com>
2016-05-19 20:49:06 -0700

    generate task model and create association w/ user in db

---
+++ db/migrate/20160520034555_add_tasks_ref_to_user.rb
@@ -0,0 +1,5 @@
+class AddTasksRefToUser < ActiveRecord::Migration
+  def change
+    add_reference :users, :task, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509234315) do
+ActiveRecord::Schema.define(version: 20160520034555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "description", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                          null: false
@@ -30,10 +37,13 @@ ActiveRecord::Schema.define(version: 20160509234315) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.integer  "task_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["task_id"], name: "index_users_on_task_id", using: :btree
 
+  add_foreign_key "users", "tasks"
 end
